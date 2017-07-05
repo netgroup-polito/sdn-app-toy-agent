ae5b74ccbf87f08845c9c7f468123d1af8a03704
Srini Seetharaman <seethara@stanford.edu>
2017-07-05 06:12:39 -0700

    SSSA State Listener

---
+++ src/SOON/StateListener/CommandMsg.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StateListener;
+
+/**
+ *
+ * @author lara
+ */
+class CommandMsg {
+    Long id;
+    command act;
+    String var;
+    String objret;
+    Object obj;
+    public enum command{GET, CONFIG, DELETE};
+}
